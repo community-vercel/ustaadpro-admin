def0a0cce215df8c89c8b4e7264c35e13f38ad85
Anisul Islam <anis.inbox10@gmail.com>
2026-07-02 22:19:37 +0500

    added prod url

--- src/app/api/admin-login/route.ts
+++ src/app/api/admin-login/route.ts
@@ -7,7 +7,7 @@ import {
 } from '@/lib/adminSession';
 
 const API_BASE_URL =
-  process.env.NEXT_PUBLIC_API_BASE_URL || 'http://localhost:5000/api';
+  process.env.NEXT_PUBLIC_API_BASE_URL || 'https://api.ustaadpro.pk/api';
 
 export async function GET(request: NextRequest) {
   const isAuthenticated = await isValidAdminSessionToken(
